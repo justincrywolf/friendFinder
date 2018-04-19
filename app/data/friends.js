b9a4ecce55e49926471aa14ea5f9216fb248604e
justincrywolf <justincrywolf@gmail.com>
2018-04-19 18:34:36 -0400

    Finally got modal working

---
+++ app/data/friends.js
@@ -0,0 +1,38 @@
+var friends = [
+    {
+        name: "Katya Zamolodchikova",
+        photo: "https://ih0.redbubble.net/image.430746115.7375/flat,800x800,075,f.u1.jpgf",
+        scores: [
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+        ]
+    },
+    {
+        name: "Trinity Taylor",
+        photo: "https://hotspotsmagazine.com/wp-content/uploads/2017/07/3228_QA_Trinity-Taylor-Talks-RuPauls-Drag-Race.jpg",
+        scores: [
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+        ]
+    },
+    {
+        name: "Manila Luzon",
+        photo: "http://sanfrancisco.carpediem.cd/data/afisha/o/89/e0/89e0413774.jpg",
+        scores: [
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+        ]
+    }
+
+];
+
+module.exports = friends;
---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,12 @@
+console.log("htmlRoutes connected");
+var path = require("path");
+
+module.exports = function (app){
+    app.get("/survey", function(req, res){
+        res.sendFile(path.join(__dirname + "/../public/survey.html"));    
+    });
+
+    app.use(function(req, res){
+        res.sendFile(path.join(__dirname + "/../public/home.html"));
+    })
+}
